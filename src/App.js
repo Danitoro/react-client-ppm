0811c452231111534495e47cdda5cbe732008bd2
toroflod <daniel.toro_flores@roche.com>
2020-03-30 12:46:23 +0200

    reduxStuffs

--- src/App.js
+++ src/App.js
@@ -6,17 +6,21 @@ import Header from "./components/Layout/Header";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import addProject from "./components/Project/addProject";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Header />
-          <Route path="/dashboard" component={Dashboard} />
-          <Route path="/addProject" component={addProject} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Route path="/dashboard" component={Dashboard} />
+            <Route path="/addProject" component={addProject} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
